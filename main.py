accfa7746af7d415d02e7535b1533fb8cf3be483
Ronald Covington <rpcovington2@gmail.com>
2026-03-15 12:29:03 -0400

    Fix duplicate app initialization
    Remove duplicate app initialization in main block.

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import web
 # from web import CreateApp  # LIVE
 #from Dev import CreateApp   # Development
 
+app = web.CreateApp()
 
 if __name__ == '__main__':
-
-    app = web.CreateApp()
     app.run(debug=True, host='0.0.0.0', port=9000)
